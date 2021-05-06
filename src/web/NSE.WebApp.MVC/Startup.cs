7b41f8f33b53a7ebba28c498c33ec5dc2fdef788
Josemar <josemaar.sousa@gmail.com>
2021-05-06 13:21:21 -0300

    WepApp Updates

--- src/web/NSE.WebApp.MVC/Startup.cs
+++ src/web/NSE.WebApp.MVC/Startup.cs
@@ -20,6 +20,8 @@ namespace NSE.WebApp.MVC
             services.AddIdentityConfiguration();
 
             services.AddMvcConfiguration(Configuration);
+
+            services.RegisterServices();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
