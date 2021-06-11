dac0bf1cbb4b02ea8bade8d49300d592a4a41881
Josemar <josemaar.sousa@gmail.com>
2021-06-10 22:19:49 -0300

    MVC - Catalogo

--- src/web/NSE.WebApp.MVC/Configuration/WebAppConfig.cs
+++ src/web/NSE.WebApp.MVC/Configuration/WebAppConfig.cs
@@ -55,7 +55,7 @@ namespace NSE.WebApp.MVC.Configuration
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Catalogo}/{action=Index}/{id?}");
             });
         }
     }
